5f1408ca2c7ac9fa71b4165701264c13706c8e43
Muhammad Natsir hentihu <hentihu.natsir@gmail.com>
2024-02-17 16:29:37 +0700

    update add todo

--- pages/todos.vue
+++ pages/todos.vue
@@ -7,10 +7,10 @@
                     <h1 class="text-lg font-bold">Username</h1>
                     <span class="text-md">E-Mail</span>
                 </div>
-                <form class="w-full flex flex-col gap-2" @submit.prevent="">
-                    <input type="text" placeholder="Title" class="border border-slate-600 p-2">
-                    <textarea id="w3review" name="w3review" rows="4" cols="50" placeholder="Description"
-                        class="border border-slate-600 p-2"></textarea>
+                <form class="w-full flex flex-col gap-2" @submit.prevent="submitForm">
+                    <input type="text" v-model="formData.title" placeholder="Title" class="border border-slate-600 p-2">
+                    <textarea id="w3review" v-model="formData.description" name="w3review" rows="4" cols="50"
+                        placeholder="Description" class="border border-slate-600 p-2"></textarea>
                     <button class="shadow-slate-500 shadow-xl w-full rounded-lg bg-slate-300 h-auto hover:bg-slate-400">Add
                         TODO</button>
                 </form>
@@ -20,64 +20,16 @@
                     <h1 class="text-center font-black text-2xl underline">TODO List</h1>
                     <hr class="border-b border-dashed border-slate-500 my-3">
                 </div>
-                <ul class="h-full ">
-                    <li>
+                <ul class="h-full">
+                    <li v-for="todo in fix" v-bind:key="todo.id">
                         <div class="flex flex-col">
                             <div class="flex flex-row">
-                                <span class="w-2/3 max-w-full text-wrap h-10 max-h-10 leading-tight font-bold">Main Game
-                                    Bersama Kawan
-                                    Kawan</span>
-                                <span class="w-1/3 text-right">ONGOING</span>
+                                <span class="w-2/3 max-w-full text-wrap h-10 max-h-10 leading-tight font-bold">{{
+                                    todo.title
+                                }}</span>
+                                <span class="w-1/3 text-right">{{ todo.status }}</span>
                             </div>
-                            <p class="w-full text-wrap text-xs ">Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit.
-                                Fusce mattis, purus ut iaculis fermentum, dui lacus gravida dolor, in vulputate eros sem ac
-                                elit. </p>
-                        </div>
-                        <hr class="border-b-0 border-slate-400 my-1 ">
-                    </li>
-                    <li>
-                        <div class="flex flex-col">
-                            <div class="flex flex-row">
-                                <span class="w-2/3 max-w-full text-wrap h-10 max-h-10 leading-tight font-bold">Main Game
-                                    Bersama Kawan
-                                    Kawan</span>
-                                <span class="w-1/3 text-right">ONGOING</span>
-                            </div>
-                            <p class="w-full text-wrap text-xs ">Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit.
-                                Fusce mattis, purus ut iaculis fermentum, dui lacus gravida dolor, in vulputate eros sem ac
-                                elit. </p>
-                        </div>
-                        <hr class="border-b-0 border-slate-400 my-1 ">
-                    </li>
-                    <li>
-                        <div class="flex flex-col">
-                            <div class="flex flex-row">
-                                <span class="w-2/3 max-w-full text-wrap h-10 max-h-10 leading-tight font-bold">Main Game
-                                    Bersama Kawan
-                                    Kawan</span>
-                                <span class="w-1/3 text-right">ONGOING</span>
-                            </div>
-                            <p class="w-full text-wrap text-xs ">Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit.
-                                Fusce mattis, purus ut iaculis fermentum, dui lacus gravida dolor, in vulputate eros sem ac
-                                elit. </p>
-                        </div>
-                        <hr class="border-b-0 border-slate-400 my-1 ">
-                    </li>
-                    <li>
-                        <div class="flex flex-col">
-                            <div class="flex flex-row">
-                                <span class="w-2/3 max-w-full text-wrap h-10 max-h-10 leading-tight font-bold">Main Game
-                                    Bersama Kawan
-                                    Kawan</span>
-                                <span class="w-1/3 text-right">ONGOING</span>
-                            </div>
-                            <p class="w-full text-wrap text-xs ">Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit.
-                                Fusce mattis, purus ut iaculis fermentum, dui lacus gravida dolor, in vulputate eros sem ac
-                                elit. </p>
+                            <p class="w-full text-wrap text-xs ">{{ todo.description }} </p>
                         </div>
                         <hr class="border-b-0 border-slate-400 my-1 ">
                     </li>
@@ -91,4 +43,29 @@
 useHead({
     title: 'TODO - List',
 })
+
+const { data: todos } = await useFetch('http://localhost:8000/api/todo')
+const fix = todos.value.data
+
+const formData = ref({
+    id_user: 1,
+    title: '',
+    description: '',
+    status: 'TODO',
+})
+
+const submitForm = async () => {
+    const { data: responseData } = await useFetch('http://localhost:8000/api/todo', {
+        method: 'post',
+        body: {
+            id_user: formData.value.id_user,
+            title: formData.value.title,
+            description: formData.value.description,
+            status: formData.value.status,
+        }
+    })
+    console.log(responseData.value)
+    window.location.reload(true)
+}
+
 </script>

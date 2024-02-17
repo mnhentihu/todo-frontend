6eef9a8f19a7f6181dcad51aaaa1868dc153df60
Muhammad Natsir hentihu <hentihu.natsir@gmail.com>
2024-02-17 13:01:03 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="h-screen flex justify-center bg-white font-mono bg-gradient-to-tr from-cyan-300 via-blue-300 to-purple-400">
+        <div class="h-2/3 w-1/3 bg-slate-100 m-auto rounded-3xl shadow-slate-500 shadow-xl">
+            <div class="flex flex-col gap-10 items-center justify-center p-8 h-full ">
+                <div class="header text-center">
+                    <h1 class="text-xl font-bold">
+                        Welcome to TODO List!
+                    </h1>
+                    <h3>Place your ToDo List and Keep it <span>Organized!</span></h3>
+                </div>
+                <div class="form text-left">
+                    <form action="" class="flex flex-col gap-4 ">
+                        <div>
+                            <label for="username">Username</label><br>
+                            <input type="text" id="username" name="username" placeholder="Username"
+                                class="rounded-lg px-2 shadow-md"><br>
+                        </div>
+                        <div>
+                            <label for="password">Password</label><br>
+                            <input type="text" id="password" name="password" placeholder="Password"
+                                class="rounded-lg px-2 shadow-md"><br>
+                        </div>
+                        <button
+                            class="cursor-pointer bg-white w-full rounded-lg shadow-md hover:bg-slate-200">Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+  
---
+++ pages/todos.vue
@@ -0,0 +1,79 @@
+<template>
+    <main class="w-full font-mono bg-gradient-to-tr from-cyan-300 via-blue-300 to-purple-400">
+        <div class="m-auto w-3/4 h-screen flex flex-row gap-4 justify-between items-center ">
+            <div
+                class="profile w-1/2 h-2/3 bg-slate-100 rounded-l-2xl p-2 flex flex-col items-center justify-center gap-8 shadow-slate-500 shadow-xl">
+                <div class="flex flex-col items-center">
+                    <h1 class="text-lg font-bold">Username</h1>
+                    <span class="text-md">E-Mail</span>
+                </div>
+                <form class="w-full flex flex-col gap-2" @submit.prevent="">
+                    <input type="text" placeholder="Title">
+                    <textarea id="w3review" name="w3review" rows="4" cols="50" placeholder="Description"></textarea>
+                    <button class="shadow-slate-500 shadow-xl w-full rounded-lg bg-slate-300 h-auto hover:bg-slate-400">Add
+                        TODO</button>
+                </form>
+            </div>
+            <div class="todo-list w-full h-2/3 bg-slate-100 rounded-r-2xl p-6 shadow-slate-500 shadow-xl  overflow-y-auto">
+                <div class="">
+                    <h1 class="text-center font-black text-2xl underline">TODO List</h1>
+                    <hr class="border-b border-dashed border-slate-500 my-3">
+                </div>
+                <ul class="h-full ">
+                    <li>
+                        <div class="flex flex-col">
+                            <div class="flex flex-row">
+                                <span class="w-2/3 max-w-full text-wrap h-10 max-h-10 leading-tight font-bold">Main Game
+                                    Bersama Kawan
+                                    Kawan</span>
+                                <span class="w-1/3 text-right">ONGOING</span>
+                            </div>
+                            <p class="w-full text-wrap">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                                mattis,
+                                purus ut iaculis fermentum, dui lacus gravida dolor, in vulputate eros sem ac elit. </p>
+                        </div>
+                        <hr class="border-b-0 border-slate-400 my-1 ">
+                    </li>
+                    <li>
+                        <div class="flex flex-col">
+                            <div class="flex flex-row">
+                                <span class="w-2/3 max-w-full text-wrap h-10 max-h-10 leading-tight font-bold">Main Game
+                                    Bersama Kawan
+                                    Kawan</span>
+                                <span class="w-1/3 text-right">ONGOING</span>
+                            </div>
+                            <p class="w-full text-wrap">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                                mattis,
+                                purus ut iaculis fermentum, dui lacus gravida dolor, in vulputate eros sem ac elit. </p>
+                        </div>
+                        <hr class="border-b-0 border-slate-400 my-1 ">
+                    </li>
+                    <li>
+                        <div class="flex flex-col">
+                            <div class="flex flex-row">
+                                <span class="w-2/3 max-w-full text-wrap h-10 max-h-10 leading-tight font-bold">Main Game
+                                    Bersama Kawan
+                                    Kawan</span>
+                                <span class="w-1/3 text-right">ONGOING</span>
+                            </div>
+                            <p class="w-full text-wrap">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                                mattis,
+                                purus ut iaculis fermentum, dui lacus gravida dolor, in vulputate eros sem ac elit. </p>
+                        </div>
+                        <hr class="border-b-0 border-slate-400 my-1 ">
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    methods: {
+        addTodo() {
+
+        }
+    }
+}
+</script>
